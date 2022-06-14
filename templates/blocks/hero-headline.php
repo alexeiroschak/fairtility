1ddac014c2245f67dbed20443fc80b1d2a93e68a
Alexei Roschak <alexei@ascenderstudios.com>
2022-06-14 10:18:18 -0400

    - adjust padding on white-background blocks - add field to use page title for hero headline block

--- templates/blocks/hero-headline.php
+++ templates/blocks/hero-headline.php
@@ -1,5 +1,11 @@
 <div id="<?= get_field('section_id') ?>" class="block hero-headline">
     <div class="container">
-        <h1 class="<?php if (get_field('show_divider')) { echo 'has-line'; } ?>"><?= get_field('headline') ?></h1>
+        <h1 class="<?php if (get_field('show_divider')) { echo 'has-line'; } ?>">
+            <?php if (get_field('use_page_title')) : ?>
+                <?php the_title() ?>
+            <?php else : ?>
+                <?= get_field('headline') ?>
+            <?php endif ?>
+        </h1>
     </div>
 </div>
