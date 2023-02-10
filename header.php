e6057ed06bd1e333398f6f6151878a5eb03f9aa3
Alexei Roschak <alexei@ascenderstudios.com>
2023-02-10 10:29:04 -0500

    blocks gtm until cookie consent

--- header.php
+++ header.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5QMFSN6');</script>
-<!-- End Google Tag Manager -->
+
+   <?php if (isset($_COOKIE['CookieConsent'])) : ?>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5QMFSN6');</script>
+      <!-- End Google Tag Manager -->
+   <?php endif ?>
 
    <meta charset="UTF-8">
    <title><?php the_title(); ?></title>
@@ -16,10 +19,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
    <?php wp_head(); ?>
 </head>
 <body <?php body_class( get_page_slug() ); ?>>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QMFSN6"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
+   <?php if (isset($_COOKIE['CookieConsent'])) : ?>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QMFSN6"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+   <?php endif ?>
 
    <aside id="request_demo_flyout">
       <button id="request_demo_flyout__close_button">
